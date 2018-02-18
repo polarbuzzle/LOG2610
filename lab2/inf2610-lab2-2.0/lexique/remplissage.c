c2f3eb57ab03d6a5c33f668a1053db89209aee53
Samuel Ferron <saferc@l4714-22.info.polymtl.ca>
2018-02-18 17:52:58 -0500

    TP fini

--- lab2/inf2610-lab2-2.0/lexique/remplissage.c
+++ lab2/inf2610-lab2-2.0/lexique/remplissage.c
@@ -22,14 +22,12 @@ int main(int argc, char **argv) {
      * TODO: Creer un tube, puis le remplir octet par octet a l'infini
      */
     if (pipe(fds) == -1) { printf("fok\n"); }
-    close(fds[READ]);
-    while(write(fds[WRITE], &c, sizeof(c)) >= 0 ) {
+    // close(fds[READ]);
+    while(write(fds[WRITE], &c, sizeof(c)) != 0 ) {
         i++;
         printf("%d\n", i);
     }
-
-
-
+    
 
     return 0;
 }
